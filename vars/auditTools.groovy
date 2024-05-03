9832946ec1d6062a296900b432647ba88de64aeb
kaif225 <100071569+kaif225@users.noreply.github.com>
2024-05-03 13:52:52 +0530

    Update auditTools.groovy

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -2,7 +2,7 @@ def call() {
     node { 
         // Clone the repository and build the Docker image
         sh """
-            cd ${WORKSPACE} &&
+            cd ${WORKSPACE}/porject3 &&
             ls
            """
     }
