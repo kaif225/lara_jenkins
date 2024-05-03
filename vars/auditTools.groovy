e8dd7eb63261e64ccf58045633ecff1ef4be7a8b
kaif225 <100071569+kaif225@users.noreply.github.com>
2024-05-03 14:56:36 +0530

    Update auditTools.groovy

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -2,7 +2,6 @@ def call() {
     node { 
         // Clone the repository and build the Docker image
         sh """
-            cd /var/jenkins_home/workspace/porject3 &&
             ls
             touch file1
            """
