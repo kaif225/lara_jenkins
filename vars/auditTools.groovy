bc009d119678e7ffd85a06e970b5bb915685a977
kaif225 <100071569+kaif225@users.noreply.github.com>
2024-05-03 13:38:48 +0530

    Update auditTools.groovy

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -2,9 +2,8 @@ def call() {
     node { 
         // Clone the repository and build the Docker image
         sh """
-            git clone https://github.com/kaif225/lara_jenkins.git &&
-            cd ${WORKSPACE}/lara_jenkins/src &&
-            docker build -t latavel_proj .
+            cd ${WORKSPACE} &&
+            ls
            """
     }
 }
