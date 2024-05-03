7fb74b288782df1fc6028f16fd955e787523d4e9
kaif225 <100071569+kaif225@users.noreply.github.com>
2024-05-03 11:16:35 +0530

    Update auditTools.groovy

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -1,8 +1,7 @@
 def call() {
     node { 
         sh """ 
-           docker build -t laravel2_image .
-           git --version 
+           myCustomScript.myCustomFunction() 
            """
     }
 }
