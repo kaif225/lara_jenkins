41721a3d929d44d167318b40fd5abd0f90a55796
kaif225 <100071569+kaif225@users.noreply.github.com>
2024-05-03 17:52:19 +0530

    Update auditTools.groovy

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -4,12 +4,7 @@ def call(String repoUrl, String branch){
    sh "git checkout ${branch}"
    sh "cd src && ls "
    sh """
-       cd src &&
-       composer update && 
-       composer config --no-plugins allow-plugins.phpstan/extension-installer true &&
-       composer install --no-interaction --prefer-dist &&
-       cp .env.example .env
-       php artisan key:generate && vendor/bin/phpunit
+       docker build -t abc .
       """ 
    return workingDir
    
