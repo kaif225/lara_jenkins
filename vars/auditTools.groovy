0c7f3ecc1ac70bdcea203b619c06a89f2d9cf7b4
kaif225 <100071569+kaif225@users.noreply.github.com>
2024-05-03 16:55:09 +0530

    Update auditTools.groovy

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -3,6 +3,13 @@ def call(String repoUrl, String branch){
    sh "git clone ${repoUrl} ${workingDir}"
    sh "git checkout ${branch}"
    sh "cd src && ls "
+   sh """
+       curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer &&
+        composer config --no-plugins allow-plugins.phpstan/extension-installer true &&
+        composer install --no-interaction --prefer-dist &&
+        php artisan key:generate &&
+        vendor/bin/phpunit
+      """
    return workingDir
    
 }
